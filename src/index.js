5bb62e24b760f9ed9eb8dba6c83a6809d16098b9
sei <106228151+SeiUIandReact@users.noreply.github.com>
2022-05-25 20:17:20 +0900

    save

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import "./styles.css";
+/**
+ * const.letなどの変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = 'var変数';
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = '変数を上書き';
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = 'var変数を再宣言';
+// console.log(val1);
+
+// let val2 = 'let変数';
+// console.log(val2);
+
+// // let変数は上書き可能 /
+// val2 = 'ley変数を上書き';
+// console.log(val2);
+
+// // letは際宣言不可能
+// let val2 = 'let変数を際宣言';
+
+// const
+// const val3 = 'const変数';
+// console.log(val3);
+
+// // const変数は上書き不可能
+// val3 = 'const変数を上書き';
+// const val3 = 'const変数を際宣言';
+
+// オブジェクトや配列だと上書きや際宣言ができる
+// const val4 = {
+//   name: 'john',
+//   age: 28,
+// };
+// // console.log(val4);
+
+// // nameの中身を変えてみる
+// val4.name = 'Mary';
+// val4.adress = 'Paris';// 最初の定義時にはない要素を追加することもできる
+// console.log(val4);
+
+// const val5 = ['dog', 'cat'];
+// console.log(val5);
